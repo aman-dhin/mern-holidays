12d765a528ada21cb0376c96b44134bd635b79bd
aman-dhin <dhingraaman680@gmail.com>
2026-04-16 15:14:04 +0530

    hotel data changes

--- hotel-booking-frontend/src/pages/AddHotel.tsx
+++ hotel-booking-frontend/src/pages/AddHotel.tsx
@@ -1,4 +1,6 @@
-import { useMutation } from "react-query";
+import { useMutation, } from "react-query";
+import {  useQueryClient } from "react-query";
+
 import { useNavigate } from "react-router-dom";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import useAppContext from "../hooks/useAppContext";
@@ -7,6 +9,9 @@ import * as apiClient from "../api-client";
 const AddHotel = () => {
   const { showToast } = useAppContext();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+
 
   const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
     onSuccess: () => {
@@ -16,11 +21,15 @@ const AddHotel = () => {
           "Your hotel has been added to the platform successfully! Redirecting to My Hotels...",
         type: "SUCCESS",
       });
-      // Redirect to My Hotels page after successful save
+  
+      // 🔥 MOST IMPORTANT LINE
+      queryClient.invalidateQueries(["fetchMyHotels"]);
+  
       setTimeout(() => {
         navigate("/my-hotels");
-      }, 1500); // Give user time to see the success message
+      }, 1500);
     },
+  
     onError: () => {
       showToast({
         title: "Failed to Add Hotel",
--- hotel-booking-frontend/src/pages/MyHotels.tsx
+++ hotel-booking-frontend/src/pages/MyHotels.tsx
@@ -42,7 +42,7 @@ const MyHotels = () => {
     {
       onError: () => {},
       loadingMessage: "Loading your hotels...",
-      enabled: isLoggedIn,
+      enabled: false,
     },
   );
 
